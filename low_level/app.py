0d06af702b419b9937912d13b68960110e09e7db
Rifo Ahmad Genadi <rifoagenadi@gmail.com>
2019-09-05 12:41:41 +0700

    cleanup markdown format

--- low_level/app.py
+++ low_level/app.py
@@ -4,21 +4,11 @@ import signal
 import pyuv
 import os
 
-# Open HTML Files
-with open(os.path.dirname(os.path.abspath(__file__)) + '/../files/large.html') as f:
-    large_contents = f.read()
-    f.close() 
-
-with open(os.path.dirname(os.path.abspath(__file__)) + '/../files/small.html') as f:
-    small_contents = f.read()
-    f.close() 
-
 def readFile(path):
-    global large_contents, small_contents
-    if (path[1:] == "small.html") :
-        return small_contents
-    elif (path[1:] == "large.html") :
-        return large_contents
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/../files/' + path[1:]) as f:
+        content = f.read()
+        f.close()
+        return content
 
 def on_read(client, data, error):
     if data is None:
@@ -27,14 +17,16 @@ def on_read(client, data, error):
         return
     data_str = str(data)
     content = readFile(data_str.split()[1])
-    response = 'HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: closed\nContent-Length: {}\n\n{}'.format(len(content), content)
-    client.write(response.encode())
+    response = 'HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: {}\n\n{}'.format(len(content), content)
+    client.write(bytes(response, 'utf-8'))
+
 
 def on_connection(server, error):
     client = pyuv.TCP(server.loop)
     server.accept(client)
     clients.append(client)
     client.start_read(on_read)
+
 
 def signal_cb(handle, signum):
     [c.close() for c in clients]
@@ -48,7 +40,7 @@ loop = pyuv.Loop.default_loop()
 clients = []
 
 server = pyuv.TCP(loop)
-server.bind(("127.0.0.1", 1234))
+server.bind(("0.0.0.0", 1234))
 server.listen(on_connection)
 
 signal_h = pyuv.Signal(loop)
